c75e401042638e07de3bc9ca5ff6ba428e488f43
zhongshijie <zhongshijie1995@outlook.com>
2021-11-30 00:01:02 +0800

    初始化仓库

---
+++ 00-MySQL-DBA.sql
@@ -0,0 +1,8 @@
+-- 创建用户
+create user 'big_sql'@'%' identified by 'big_sql';
+create schema big_sql default character set utf8mb4 collate utf8mb4_general_ci;
+grant all on big_sql.* to big_sql;
+flush privileges;
+-- 删除用户
+drop user big_sql;
+drop schema big_sql;
