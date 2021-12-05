9aca9e07a6d30615d7a327c80e62cc038cf50230
zhongshijie <zhongshijie1995@outlook.com>
2021-12-05 22:20:40 +0800

    Oracle增加表空间相关

--- 01-00-Oracle-DBA.sql
+++ 01-00-Oracle-DBA.sql
@@ -1,5 +1,13 @@
+-- 创建表空间
+create tablespace big_oracle
+    datafile 'C:\Users\zhongshijie\Desktop\Oracle\01-table_space\big_oracle.dbf'
+    size 128M AUTOEXTEND ON ;
 -- 创建用户
-create user big_oracle identified by big_oracle;
+create user big_oracle identified by big_oracle default tablespace big_oracle ;
 grant connect, resource to big_oracle;
+-- 赋予用户表空间权限
+alter user big_oracle quota unlimited on big_oracle;
 -- 删除用户
-drop user big_oracle cascade;
+drop user big_oracle cascade ;
+-- 删除表空间
+drop tablespace big_oracle including contents and datafiles cascade constraints ;
