c403e3bf2671227c978e9f1a88fbeb3394f75211
钟世杰 <zhongshijie1995@outlook.com>
2022-03-11 21:21:34 +0800

    更新20220311

--- 00-00-MySQL-DBA.sql
+++ 00-00-MySQL-DBA.sql
@@ -2,11 +2,23 @@
 show databases ;
 -- 查询用户
 select * from mysql.user;
+
+/* 创建big_boot_test */
 -- 创建用户
-create user 'big_sql'@'%' identified by 'big_sql';
-create schema big_sql default character set utf8mb4 collate utf8mb4_general_ci;
-grant all on big_sql.* to big_sql;
+create user 'big_boot_test'@'%' identified by 'big_boot_test';
+create schema big_boot_test default character set utf8mb4 collate utf8mb4_general_ci;
+grant all on big_boot_test.* to big_boot_test;
 flush privileges;
 -- 删除用户
-drop user big_sql;
-drop schema big_sql;
+drop user big_boot_test;
+drop schema big_boot_test;
+
+/* 创建big_boot_prod */
+-- 创建用户
+create user 'big_boot_prod'@'%' identified by 'big_boot_prod';
+create schema big_boot_prod default character set utf8mb4 collate utf8mb4_general_ci;
+grant all on big_boot_prod.* to big_boot_prod;
+flush privileges;
+-- 删除用户
+drop user big_boot_prod;
+drop schema big_boot_prod;
--- 01-00-Oracle-DBA.sql
+++ 01-00-Oracle-DBA.sql
@@ -1,10 +1,10 @@
 -- 创建表空间
 create tablespace big_oracle
-    datafile 'C:\Users\zhongshijie\Desktop\Oracle\01-table_space\big_oracle.dbf'
+    datafile 'C:\Users\zhongshijie\Desktop\DataSpace\big_oracle.dbf'
     size 128M AUTOEXTEND ON ;
 -- 创建用户
 create user big_oracle identified by big_oracle default tablespace big_oracle ;
-grant connect, resource to big_oracle;
+grant connect, resource, create any view to big_oracle;
 -- 赋予用户表空间权限
 alter user big_oracle quota unlimited on big_oracle;
 -- 删除用户
