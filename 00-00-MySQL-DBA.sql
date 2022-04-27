a4ef99a7313ac5ff3f956e9b83c272bac948ad59
zhongshijie <zhongshijie1995@outlook.com>
2022-04-27 09:52:04 +0800

    20220427

--- 00-00-MySQL-DBA.sql
+++ 00-00-MySQL-DBA.sql
@@ -3,7 +3,7 @@ show databases ;
 -- 查询用户
 select * from mysql.user;
 
-/* 创建big_boot_test */
+/* 创建 big_boot_test */
 -- 创建用户
 create user 'big_boot_test'@'%' identified by 'big_boot_test';
 create schema big_boot_test default character set utf8mb4 collate utf8mb4_general_ci;
@@ -13,7 +13,7 @@ flush privileges;
 drop user big_boot_test;
 drop schema big_boot_test;
 
-/* 创建big_boot_prod */
+/* 创建 big_boot_prod */
 -- 创建用户
 create user 'big_boot_prod'@'%' identified by 'big_boot_prod';
 create schema big_boot_prod default character set utf8mb4 collate utf8mb4_general_ci;
@@ -22,3 +22,24 @@ flush privileges;
 -- 删除用户
 drop user big_boot_prod;
 drop schema big_boot_prod;
+
+
+/* 创建 big_api_test */
+-- 创建用户
+create user 'big_api_test'@'%' identified by 'big_api_test';
+create schema big_api_test default character set utf8mb4 collate utf8mb4_general_ci;
+grant all on big_api_test.* to big_api_test;
+flush privileges;
+-- 删除用户
+drop user big_api_test;
+drop schema big_api_test;
+
+/* 创建 big_api_prod */
+-- 创建用户
+create user 'big_api_prod'@'%' identified by 'big_api_test';
+create schema big_api_prod default character set utf8mb4 collate utf8mb4_general_ci;
+grant all on big_api_prod.* to big_api_prod;
+flush privileges;
+-- 删除用户
+drop user big_api_prod;
+drop schema big_api_prod;
