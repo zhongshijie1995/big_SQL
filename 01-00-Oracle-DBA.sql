f34f6d8a97e0a35806be14291d3f72e23ee39964
zhongshijie <zhongshijie1995@outlook.com>
2022-05-19 22:59:05 +0800

    20220519

--- 01-00-Oracle-DBA.sql
+++ 01-00-Oracle-DBA.sql
@@ -1,15 +1,30 @@
 /* 192.168.0.101:1521  system  zhongshijie */
 
--- 创建表空间
-create tablespace big_oracle
-    datafile 'D:\\big_oracle.dbf'
+-- 创建big_a
+create tablespace big_a
+    datafile 'D:\OracleDB\tab_space\big_a.dbf'
     size 128M AUTOEXTEND ON ;
 -- 创建用户
-create user big_oracle identified by big_oracle default tablespace big_oracle ;
-grant connect, resource, create any view to big_oracle;
+create user big_a identified by big_a default tablespace big_a ;
+grant connect, resource, create any view to big_a;
 -- 赋予用户表空间权限
-alter user big_oracle quota unlimited on big_oracle;
+alter user big_a quota unlimited on big_a;
 -- 删除用户
-drop user big_oracle cascade ;
+drop user big_a cascade ;
 -- 删除表空间
-drop tablespace big_oracle including contents and datafiles cascade constraints ;
+drop tablespace big_a including contents and datafiles cascade constraints ;
+
+
+-- 创建big_b
+create tablespace big_b
+    datafile 'D:\OracleDB\tab_space\big_b.dbf'
+    size 128M AUTOEXTEND ON ;
+-- 创建用户
+create user big_b identified by big_b default tablespace big_b ;
+grant connect, resource, create any view to big_b;
+-- 赋予用户表空间权限
+alter user big_b quota unlimited on big_b;
+-- 删除用户
+drop user big_b cascade ;
+-- 删除表空间
+drop tablespace big_b including contents and datafiles cascade constraints ;
